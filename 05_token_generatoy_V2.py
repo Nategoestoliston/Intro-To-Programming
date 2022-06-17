e659071828ed73e5c379e6de19532b31fa8866b0
Nategoestoliston <nate.mutuku@liston.school.nz>
2022-06-17 12:08:11 +1200

    token generator

--- 05_token_generatoy_V2.py
+++ 05_token_generatoy_V2.py
@@ -2,8 +2,16 @@ import random
 
 #main routine goes here
 tokens = ["unicorn", "horse", "zebra", "donkey"]
+balance = 100
 
 #Testing loops to generate 20 tokens
 for item in range ( 0, 20):
     chosen = random.choice(tokens)
-    print(chosen, end='\t')
+    # print(chosen, end='\t')
+
+    if chosen == "unicorn":
+        balance += 4
+    elif chosen == "donkey":
+        balance -= 1
+    else :
+        balance -=0.5
