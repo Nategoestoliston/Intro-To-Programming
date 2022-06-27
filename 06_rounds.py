23a186a93b51445c1bb482f21205798626007dcb
Nategoestoliston <nate.mutuku@liston.school.nz>
2022-06-27 13:10:18 +1200

    rounds

--- 06_rounds.py
+++ 06_rounds.py
@@ -2,11 +2,19 @@ balance = 5
 
 round_played = 0
 
-play_again = input("Press <Enter> to play...")
+play_again = input("Press <Enter> to play...").lower()
 while play_again == "":
+
     round_played += 1
+
+    print("*** Round #{} ***".format(round_played))
     print(round_played)
     balance -= 1
     print("Balance: ", balance)
     print()
-    play_again = input("Press Enter to play again or XXX to quit")
+
+    if balance < 1:
+        play_again = "xxx"
+        print("Sorry you have run out of money")
+    else:
+        play_again = input("Press Enter to play again or XXX to quit")
