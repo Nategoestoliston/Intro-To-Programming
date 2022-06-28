46b41bb41e927f51f9f72903fbd920b534d8db46
Nategoestoliston <nate.mutuku@liston.school.nz>
2022-06-28 15:07:54 +1200

    base

--- 00_LU_base.py
+++ 00_LU_base.py
@@ -1,3 +1,4 @@
+import random
 #functions go here
 def  yes_no(question):
     
@@ -50,4 +51,38 @@ if played_before == "no":
         
 #ask user how much they will play with
 how_much = num_check("How much would you like to play with ? ", 0, 10,)
-print("You will be spending ${}".format(how_much)) 
+print("You will be spending ${}".format(how_much))
+balance = 5
+
+round_played = 0
+
+play_again = input("Press <Enter> to play...").lower()
+while play_again == "":
+
+    round_played += 1
+
+    print()
+    print("*** Round #{} ***".format(round_played))
+    
+    chosen_num = random.randint(1, 100)
+    # print(chosen, end='\t')
+
+    if 1 <= chosen_num <= 5:
+        chosen = "unicorn"
+        balance += 4
+    elif 6 <= chosen_num <=36:
+        chosen = "donkey"
+        balance -= 1
+    else :
+        if chosen_num % 2 == 0:
+            chosen = "horse"
+        else :
+            chosen = "zebra"
+        balance -=0.5
+    print("You got a {}. Your balance is ${:.2f}".format(chosen, balance))
+
+    if balance < 1:
+        play_again = "xxx"
+        print("Sorry you have run out of money")
+    else:
+        play_again = input("Press Enter to play again or XXX to quit") 
