ddd6a74a05bcd4cb663ea15f3a799c417ed66b89
Nategoestoliston <nate.mutuku@liston.school.nz>
2022-06-28 17:11:54 +1200

    sandbox

--- sandbox.py
+++ sandbox.py
@@ -1,24 +1,34 @@
-# # ask users for a number
-# get_number = int(input("Choose a number"))
+# # # ask users for a number
+# # get_number = int(input("Choose a number"))
 
-# # Multiply the number by 5
-# times_five = get_number * 5
+# # # Multiply the number by 5
+# # times_five = get_number * 5
 
-# # Output the result
-# print("{} times five is equal to "
-# "{}".format(get_number, times_five))
+# # # Output the result
+# # print("{} times five is equal to "
+# # "{}".format(get_number, times_five))
 
-# for item in range (1, 10):
-#     print(item)
+# # for item in range (1, 10):
+# #     print(item)
 
-# options = ["unicorn", "horse", "zebra" , "donkey"]
-# for item in options:
-#     print(item)
+# # options = ["unicorn", "horse", "zebra" , "donkey"]
+# # for item in options:
+# #     print(item)
 
-name = ""
-while name.lower() != "xxx":
-    name = input ("Who are you? ")
-    print(name)
+# name = ""
+# while name.lower() != "xxx":
+#     name = input ("Who are you? ")
+#     print(name)cls
+# print()
+# print("We are done!")
 
-print()
-print("We are done!")
+greeting = "hello world"
+sides = "*" * 3
+greeting = "{} {} {}".format(sides, greeting, sides)
+top_bottom = "*" * len(greeting)
+
+print(top_bottom)
+print(greeting)
+print(top_bottom)
+
+
