a16be30db8892b1c1240275d953fb1cb3d7afdbf
Nategoestoliston <nate.mutuku@liston.school.nz>
2022-07-01 11:58:21 +1200

    user choice

---
+++ RPS Game]/01_user_choice_V2.py
@@ -0,0 +1,26 @@
+from distutils.log import error
+from urllib import response
+
+
+def choice_checker(question) :
+    error = "Please choose from rock / paper / scissors (or xxx to quit)"
+
+    valid = False
+    while not valid :
+
+        response = input(question).lower()
+        if response == "r" or response == "rock":
+            return response
+        elif response == "p" or response == "paper":
+            return response
+        elif response == "s" or response == "scissors":
+            return response
+        elif response == "xxx" :
+            return response
+        else:
+            print(error)
+
+user_choice = ""
+while user_choice != "xxx":
+    user_choice = choice_checker("Choose rock / paper / scissors (r/p/s) :")
+    print("you chose {}".format(user_choice))
