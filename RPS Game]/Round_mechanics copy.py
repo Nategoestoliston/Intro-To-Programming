5bfc303126b9aede33c70235c51ee606c70960ef
Nategoestoliston <nate.mutuku@liston.school.nz>
2022-07-04 12:10:21 +1200

    rounds

---
+++ RPS Game]/Round_mechanics copy.py
@@ -0,0 +1,19 @@
+from urllib import response
+
+def check_rounds():
+
+    while True:
+        response = input("How many rounds: ")
+
+        round_error = "Please type either <enter>  \ or an integer that is more than 0"
+        if response != "":
+            try:
+                response = int(response)
+                
+                if response < 1:
+                    print(round_error)
+                    continue
+            except ValueError:
+                print(round_error)
+                continue
+        return response
