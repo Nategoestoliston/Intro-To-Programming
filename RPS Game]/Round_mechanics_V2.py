18160236a6f52bd609392b0bbe69e1580ad00a17
Nategoestoliston <nate.mutuku@liston.school.nz>
2022-07-05 12:46:47 +1200

    rounds

---
+++ RPS Game]/Round_mechanics_V2.py
@@ -0,0 +1,47 @@
+from turtle import heading
+from urllib import response
+
+def check_rounds():
+
+    while True:
+        response = input("How many rounds: ")
+
+        round_error = "Please type either <enter>  \ or an integer that is more than 0"
+        if response != "":
+            try:
+                response = int(response)
+                
+                if response < 1:
+                    print(round_error)
+                    continue
+            except ValueError:
+                print(round_error)
+                continue
+        return response
+
+rounds_played = 0
+choose_instruction = "Please choose rock (r), paper (p), scissors (s)"
+
+rounds = check_rounds()
+
+end_game = "no"
+while end_game =="no":
+    print()
+    if rounds =="":
+        heading = "Continuous Mode : Round {}".format(rounds_played + 1)
+        print(heading)
+        choose = input("{} or 'xxx' to end: ".format(choose_instruction))
+        if choose == "xxx":
+            break
+    else :
+        heading = "Round {} of {}".format(rounds_played + 1, rounds)
+        print(heading)
+        choose = input(choose_instruction)
+        if rounds_played == rounds - 1:
+            end_game = "yes"
+
+
+    print("You chose {}".format(choose))
+
+    rounds_played += 1
+print("Thank you for playing") 
