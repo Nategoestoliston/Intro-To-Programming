a68d1d2514f032c04e7b925d70a52034c4de2a76
Nategoestoliston <nate.mutuku@liston.school.nz>
2022-07-05 13:10:35 +1200

    base

--- RPS Game]/base_component.py
+++ RPS Game]/base_component.py
@@ -1,3 +1,20 @@
+def check_rounds():
+
+    while True:
+        response = input("How many rounds: ")
+
+        round_error = "Please type either <enter>  \ or an integer that is more than 0"
+        if response != "":
+            try:
+                response = int(response)
+                
+                if response < 1:
+                    print(round_error)
+                    continue
+            except ValueError:
+                print(round_error)
+                continue
+        return response
 
 yes_no_list = ["yes", "no"]
 rps_list = ["rock", "[paper", "scissors", "xxx"]
