79d2b236993d06fb181875cbc53b68b12e445f69
Nategoestoliston <nate.mutuku@liston.school.nz>
2022-07-08 10:28:51 +1200

    base

--- RPS Game]/base_component.py
+++ RPS Game]/base_component.py
@@ -1,3 +1,4 @@
+#functions go here
 def check_rounds():
 
     while True:
@@ -16,5 +17,38 @@ def check_rounds():
                 continue
         return response
 
+#Main routine goes here
+
+#Lists of valid response
 yes_no_list = ["yes", "no"]
 rps_list = ["rock", "paper", "scissors", "xxx"]
+
+#ask user if they have played before
+#if yes, show instructions
+
+#ask user for # rounds then loop
+rounds_played = 0
+choose_instruction = "Please choose rock (r), paper (p), scissors (s)"
+
+rounds = check_rounds()
+
+end_game = "no"
+while end_game =="no":
+    print()
+    if rounds == "":
+        heading = "Continuons mode : Round {}".format(rounds_played + 1)
+
+    else :
+        heading = "Round {} of {}".format(rounds_played + 1, rounds)
+    print(heading)
+    choose = input("{} or 'xxx' to end: ".format(choose_instruction))
+    print("You choose {}".format(choose))
+
+    if choose == "xxx":
+        break
+
+    rounds_played += 1
+print("Thank you for playing") 
+
+#ask user if they want to see their game history
+#if yes, show instructions
